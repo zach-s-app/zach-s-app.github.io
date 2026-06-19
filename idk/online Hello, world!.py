293b8cd7debb0287a10cb2037b59c267f16526a5
zach <zach@zach-pi.ht.home>
2026-06-19 16:48:25 -0500

    online hello world idk why just saw games going to online but did not want to make mine so i did it to a hello world

---
+++ idk/online Hello, world!.py
@@ -0,0 +1,9 @@
+import hashlib
+import hyperlink
+import subprocess
+print("\n\x8D"+"the first Hello world to need internet since all games now a days do lol XD"+"\x99")
+b="https://raw.githubusercontent.com/zach-s-app/zach-s-app.github.io/refs/heads/main/idk/"
+a=subprocess.getoutput("curl --silent https://raw.githubusercontent.com/zach-s-app/zach-s-app.github.io/refs/heads/main/idk/lib1.py")
+c=((eval(a)+-5.5+eval(a)/0.1-54)*0.1)
+d=subprocess.getoutput(f"curl --silent {b}{c}")
+print(d+"\x98"+"\n\x99")
